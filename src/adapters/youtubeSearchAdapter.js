c3afea4dcdbd678c7e843733e8e1bcb2d0003631
ihollander <ianholla@gmail.com>
2018-12-07 11:54:49 -0500

    Add reference to config var

--- src/adapters/youtubeSearchAdapter.js
+++ src/adapters/youtubeSearchAdapter.js
@@ -18,7 +18,7 @@ class YouTubeSearchAPIAdapter {
       part: 'snippet',
       type: 'video',
       maxResults: 50,
-      key: apiKeys.youtube,
+      key: youtube,
       regionCode: 'US',
       safeSearch: 'strict',
       videoSyndicated: true
